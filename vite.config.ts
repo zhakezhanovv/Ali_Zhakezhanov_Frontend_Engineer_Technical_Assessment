d0cddd35eda04b020772e79332d00f7e0a9327d5
zhakezhanovv <zhakezhanovali01@gmail.com>
2023-07-03 21:34:41 +0600

    chore: add vite-pwa and manifest to pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import tsConfigPaths from 'vite-tsconfig-paths'
+
+import { manifest } from './manifest'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), tsConfigPaths()],
+	plugins: [react(), tsConfigPaths(), VitePWA(manifest)],
 })
