511775ca921ad823e1aa2b5df104481a28a659cd
zhakezhanovv <zhakezhanovali01@gmail.com>
2023-07-05 21:09:59 +0600

    chore(shared/assets): add images

--- src/shared/assets/images/index.ts
+++ src/shared/assets/images/index.ts
@@ -1,1 +1,4 @@
 export { default as Logo } from './logo.png'
+export { default as PlayIcon } from './play_icon.png'
+export { default as PauseIcon } from './pause_icon.png'
+export { default as CloseIcon } from './close_icon.png'
